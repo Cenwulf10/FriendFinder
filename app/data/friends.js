0bcff610b5af6381ebf102ee8fb3e4277cf334ff
Jose Gavilanes <joselgavilanes@yahoo.com>
2018-10-15 22:16:07 -0400

    add files

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+  {
+    name:"Jose Gavilanes",
+    profilePic: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/14666142_10211166257735742_5672041246626252684_n.jpg?_nc_cat=103&oh=44c49a345024f5a593c87f5547b98837&oe=5C4784BA",
+    scores: [
+      "3",
+      "2",
+      "2",
+      "4",
+      "3",
+      "5",
+      "3",
+      "3",
+      "5",
+      "5"
+    ]
+  }
+]
+
+module.exports = friends;
